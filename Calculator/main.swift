fd3efc1c7beff29ea9f8a3369b4d076294eeb6fd
borisiryshkov <boris.iryshkov@gmail.com>
2025-11-02 20:58:49 +0300

    A simple calculator in Swift

--- Calculator/main.swift
+++ Calculator/main.swift
@@ -7,5 +7,57 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Welcome to calculator!\nSelect operation: +, -, * or /")
 
+var operation: String? = readLine()
+
+switch operation {
+case "+", "-", "*", "/": break
+case nil:
+    print("Unknown operation")
+    exit(0)
+default:
+    print("Input error - input should be one of: +, -, *, /")
+}
+
+print("Input first integer")
+
+var firstNumber: Int?
+
+if let input = readLine(), let value = Int(input) {
+    firstNumber = value
+} else {
+    print("Input error - enter an integer")
+    exit(0)
+}
+
+print("Input second integer")
+
+var secondNumber: Int?
+
+if let input = readLine(), let value = Int(input) {
+    secondNumber = value
+    if operation == "/" && value == 0 {
+        print("Zero division error")
+        exit(0)
+    }
+} else {
+    print("Input error - enter an integer")
+    exit(0)
+}
+
+print("Calculating: \(firstNumber!) \(operation!) \(secondNumber!)")
+
+let result: Int
+
+switch operation {
+case "+": result = firstNumber! + secondNumber!
+case "-": result = firstNumber! - secondNumber!
+case "*": result = firstNumber! * secondNumber!
+case "/": result = firstNumber! / secondNumber!
+default :
+    print("Unknown operation")
+    exit(0)
+}
+
+print("Result: \(result)")
